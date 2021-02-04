6d008684f98b61f499e1ba04b2a629604c25ff5d
mohammj <babakas76@gmail.com>
2021-02-04 10:39:05 +0100

    4FEB

--- XGB_Classifier_decision_tree.py
+++ XGB_Classifier_decision_tree.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import plot_confusion_matrix
 import sys
 import evaluator
+import hyperparameter_tuner
 from sklearn.metrics import roc_curve
 import seaborn as sns
 import xgboost as xgb
@@ -20,7 +21,7 @@ from xgboost import XGBClassifier
 # sns.set()
 
 # file_to_read = open('.\\pickle\\preprocessed_dataset.pickle', "rb")
-file_to_read = open('/home/mohammed/pickle/preprocessed_dataset_n500.pickle', "rb")
+file_to_read = open('/home/mohammed/pickle/preprocessed_dataset_n1000.pickle', "rb")
 loaded_object = pickle.load(file_to_read)
 file_to_read.close()
 # print(loaded_object)
@@ -37,24 +38,25 @@ result = dataset["result"]
 n_result = dataset["n_result"]
 print("result : \n", result)
 print("n_result : \n", n_result)
-print("result shape : \n", result.shape)
+print("result shapae : \n", result.shape)
 
 # clf = XGBClassifier()
 y_dataset = result.loc[:, ["label"]]
 x_dataset = result.drop(["label"], axis=1)
 n_x_dataset = n_result.drop(["label"], axis=1)
-max_min_mean_median_std = n_x_dataset.drop(['quantile1', 'quantile2', 'quantile3'], axis=1)
+max_min_mean_median_std = x_dataset.drop(['quantile1', 'quantile2', 'quantile3'], axis=1)
 max_min_mean = x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'median', 'std'], axis=1)
-max_min_mean_std = n_x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'median'], axis=1)
-max_min_mean_median = n_x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'std'], axis=1)
-mean_median_std = n_x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'min', 'max'], axis=1)
-min_max = n_x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'median', 'std', 'mean'], axis=1)
-mean_std = n_x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'median', 'min', 'max'], axis=1)
-median_std = n_x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'mean', 'min', 'max'], axis=1)
-min_max_median = n_x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'std', 'mean'], axis=1)
+max_min_mean_std = x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'median'], axis=1)
+max_min_mean_median = x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'std'], axis=1)
+mean_median_std = x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'min', 'max'], axis=1)
+min_max = x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'median', 'std', 'mean'], axis=1)
+mean_std = x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'median', 'min', 'max'], axis=1)
+median_std = x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'mean', 'min', 'max'], axis=1)
+min_max_median = x_dataset.drop(['quantile1', 'quantile2', 'quantile3', 'std', 'mean'], axis=1)
+
 # print("x_dataset : ", x_dataset)
 # print("y_dataset : ", y_dataset)
-x_train, x_test, y_train, y_test = train_test_split(max_min_mean_std,
+x_train, x_test, y_train, y_test = train_test_split(mean_median_std,
                                                     y_dataset,
                                                     test_size=0.25,
                                                     shuffle=True,
@@ -62,15 +64,18 @@ x_train, x_test, y_train, y_test = train_test_split(max_min_mean_std,
                                                     random_state=42)
 # print("x_train : ", x_train)
 # print("x_test : ", x_test)
-clf = xgb.sklearn.XGBClassifier(n_estimators=100,
-                                max_depth=10,
-                                learning_rate=0.1,
-                                subsample=0.8,
-                                colsample_bytree=0.5,
+clf = xgb.sklearn.XGBClassifier(n_estimators=20,
+                                max_depth=3,
+                                learning_rate=0.3,
+                                subsample=0.9,
+                                colsample_bytree=1.0,
                                 booster="gbtree",
                                 eval_metric="error",
-                                scale_pos_weight=5,
-                                seed=42)
+                                verbosity= 0,
+                                n_jobs= -1)
+# clf = xgb.sklearn.XGBClassifier(n_estimators=100, booster="gblinear")
+# params = hyperparameter_tuner.xgb_hyperparameter_tuner(clf, x_train, y_train)
+# clf.set_params(**params)
 clf.fit(x_train, y_train)
 # xgb_pred = clf.predict(x_test)
 
