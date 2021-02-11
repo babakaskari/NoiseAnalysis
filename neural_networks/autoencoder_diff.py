48d9fabc198e22940e8f278109feb3957409d117
yoda <Siamak>
2021-02-11 13:33:34 +0330

    removing logs

--- neural_networks/autoencoder_diff.py
+++ neural_networks/autoencoder_diff.py
@@ -105,3 +105,20 @@ visualiser.train_val_loss_plotter(history)
 x_valid_predictions = autoencoder.predict(x_valid)
 visualiser.precision_recall_plotter(x_valid, x_valid_predictions, df_valid["label"], history)
 
+test_x_predictions = autoencoder.predict(df_test_x_rescaled)
+mse = np.mean(np.power(df_test_x_rescaled - test_x_predictions, 2), axis=1)
+error_df_test = pd.DataFrame({'Reconstruction_error': mse,
+                        'True_class': df_test['y']})
+error_df_test = error_df_test.reset_index()
+threshold_fixed = 0.4
+groups = error_df_test.groupby('True_class')
+fig, ax = plt.subplots()
+for name, group in groups:
+    ax.plot(group.index, group.Reconstruction_error, marker='o', ms=3.5, linestyle='',
+            label= "Break" if name == 1 else "Normal")
+ax.hlines(threshold_fixed, ax.get_xlim()[0], ax.get_xlim()[1], colors="r", zorder=100, label='Threshold')
+ax.legend()
+plt.title("Reconstruction error for different classes")
+plt.ylabel("Reconstruction error")
+plt.xlabel("Data point index")
+plt.show();
